2fe1f3aadce88c804b72f820ec102c009093536b
Nantawat Oum Chanloy <63310224@go.buu.ac.th>
2021-01-26 15:12:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <body>
 
 <?php
-echo "Nantawat Chanloy";
-echo     "Burapha University IT-25";
-echo      "ง่วงโว้ยย ตื่นตั้งแต่ตี5 ปวดหัว :<";
+echo "Nantawat Chanloy /r/n
+  Burapha University IT-25 /r/n
+  ง่วงโว้ยย ตื่นตั้งแต่ตี5 ปวดหัว :<";
 ?>
 
 </body>
